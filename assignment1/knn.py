0088d85fec0f979399757f38d844c358c707b137
Alireza Rouzitalab <alireza11591@gmail.com>
2019-06-18 14:42:22 -0400

    Half of KNN done

---
+++ assignment1/knn.py
@@ -0,0 +1,60 @@
+# Run some setup code for this notebook
+
+import random
+import numpy as np
+from assignment1.cs231n.data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Load the raw CIFAR-1 data.
+cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+
+# Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
+try:
+    del X_train, y_train
+    del X_test, y_test
+    print('Clear previously loaded data.')
+except:
+    pass
+
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+# Visualize some examples from the dataset.
+# We show a few examples of training images from each class.
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+num_classes = len(classes)
+samples_per_clas = 7
+for y, cls in enumerate(classes):
+    idxs = np.flatnonzero(y_train == y)
+    idxs = np.random.choice(idxs, samples_per_clas, replace=False)
+    for i, idx in enumerate(idxs):
+        plt_idx = i * num_classes + y + 1
+        plt.subplot(samples_per_clas, num_classes, plt_idx)
+        plt.imshow(X_train[idx].astype('uint8'))
+        plt.axis('off')
+        if i == 0:
+            plt.title(cls)
+plt.show()
+
+#Subsample the data for more efficient code execution in this exercise
+num_training = 5000
+mask = list(range(num_training))
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 500
+mask = list(range(num_test))
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+# Reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
